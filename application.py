7bf5febdf46eecccce3ca10d51c11ba5c0963630
Raymw1 <rayan.rmw@gmail.com>
2021-09-22 17:50:24 -0300

    feature: Register page basics done

--- application.py
+++ application.py
@@ -7,3 +7,7 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 @app.route("/")
 def index():
   return render_template("index.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
