b2fbb2a9ddf1e73b747f29a5d473f51e6d302bf9
Raymw1 <rayan.rmw@gmail.com>
2021-09-23 18:53:53 -0300

    feature: login logic done

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from models.User import createUser
+from models.User import createUser, getUser
 import os
 from tempfile import mkdtemp
 from flask import Flask, redirect, render_template, request, session, Response
@@ -6,6 +6,7 @@ from models import database
 from models.validators import checkUsers
 from helpers import apology
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -40,18 +41,40 @@ def register():
     username = request.form.get("username")
     password = request.form.get("password")
     passwordRepeat = request.form.get("passwordRepeat")
+    data = {"email": email, "username": username}
     if not email or not username or not password or not passwordRepeat:
-      return apology("register.html", "Please, fill all fields!", 400, {"email": email, "username": username})
+      return apology("register.html", "Please, fill all fields!", 400, data)
     if password != passwordRepeat:
-      return apology("register.html", "Passwords didn't match!", 400, {"email": email, "username": username})
+      return apology("register.html", "Passwords didn't match!", 400, data)
     if checkUsers(email, username):
-      return apology("register.html", "User already registered!", 400, {"email": email, "username": username})
+      return apology("register.html", "User already registered!", 400, data)
     user_id = createUser(email, username, password);
     session["user_id"] = user_id
-    return render_template("index.html")
+    return render_template("birthdays.html")
   else:
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-  return render_template("login.html")
+  if request.method == "POST":
+    email = request.form.get("email")
+    password = request.form.get("password")
+    data = {"email": email}
+    user = getUser(email)[0]
+    if not email or not password:
+      return apology("login.html", "Please, fill all fields!", 400, data)
+    if not user:
+      return apology("login.html", "User not registered!", 400, data)
+    if not check_password_hash(user["password"], password):
+      return apology("login.html", "Invalid password!", 401 , data)
+    session["user_id"] = user["id"]
+    return render_template("birthdays.html")
+  else:
+    return render_template("login.html")
+
+@app.route("/birthdays", methods=["GET", "POST"])
+def birthdays():
+  if request.method == "POST":
+    return render_template("birthdays.html")
+  else:
+    return render_template("birthdays.html")
--- models/User.py
+++ models/User.py
@@ -7,3 +7,6 @@ def createUser(email, username, password):
   password = generate_password_hash(password)
   user_id = db.execute("INSERT INTO users (email, username, password) VALUES (?, ?, ?);", email, username, password)
   return user_id;
+
+def getUser(email):
+  return db.execute("SELECT * FROM users WHERE email = ?", email);
